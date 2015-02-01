85376755a5b09658dd95b900f56b816b1116c4ec
Subir <subirshakya@users.noreply.github.com>
2015-02-01 12:32:48 -0600

    Hillclimber using recursion

---
+++ Hillclimber.py
@@ -0,0 +1,61 @@
+from __future__ import division
+import random
+
+def multiplier(num1, num2=1):
+	""" Multiplies all consecutive numbers between num1 and num2 """
+	start = max(num1, num2) #Get larger number
+	stop = min(num1, num2) #Get smaller number
+	product = stop #Initialize product as 1
+	while int(start) != int(stop): #Loop while larger number is not equal to one less than smaller number
+		product *= start #Multiply the results
+		start -= 1 #Decrease larger number by 1
+	return product #Return product
+		
+def bincoeffb(n, k):
+	""" Calculate binomial coefficient (factoring first) from n sets choosing k subsets"""
+	try:
+		coeff = multiplier(n, n-k+1)/multiplier(k)
+	except:
+		coeff = 0
+	return coeff
+	
+def bernoulli(k, n, p):
+	"""Calculate a Probability Mass Function (PMF) of a binomial distribution with k successes in n Bernoulli trials with probability p"""
+	PMF = bincoeffb(n, k)*pow(p,k)*pow((1-p),(n-k))
+	return PMF
+
+def hillclimb(data, prob, likeli=0, diff = 0.1, oldvalue=1):
+	"""Calculate maximum likelihood for a dataset"""
+	while diff >= 0.001:
+		if likeli == oldvalue:
+			likeli = likeli
+			prob = prob
+			diff = diff/5
+			oldvalue = 1
+			hillclimb(data, prob=prob, likeli=likeli, diff=diff, oldvalue=oldvalue)
+		else:
+			oldvalue = likeli
+			problist = [prob-diff, prob, prob+diff]
+			valuelist = [bernoulli(data[0], data[1], prob-diff), bernoulli(data[0], data[1], prob), bernoulli(data[0], data[1], prob+diff)]
+			likeli = max(valuelist)
+			prob = problist[valuelist.index(likeli)]
+			hillclimb(data, prob=prob, likeli=likeli, diff=diff, oldvalue=oldvalue)
+	return likeli, prob
+	
+"""Enter data below as pass and fail separated by commas"""
+data = ["pass","pass","pass","pass","pass"]
+data = [y.lower() for y in data]
+dataval = data.count("pass")
+numTrials = len(data)
+data = (dataval, numTrials)
+
+if data[0] == data[1]:
+	maxlikeli,  probability = 1.0, 1.0
+elif data[0] == 0:
+	maxlikeli, probability = 1.0, 0.0
+else:
+	startlist = [y/10 for y in range(0,10,1)]
+	berns = [bernoulli(data[0],data[1], y) for y in startlist]
+	prob = startlist[berns.index(random.choice(berns))]
+	maxlikeli, probability = hillclimb(data, prob)
+print "The maximum likelihood for given data is {0} for probability of {1}".format(round(maxlikeli, 5), round(probability, 5))

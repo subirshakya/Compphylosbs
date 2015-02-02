87123ac68aae9d5e146a7a47851b395b9efe0562
Subir <subirshakya@users.noreply.github.com>
2015-02-02 17:31:14 -0600

    Updated

--- Likelihood.py
+++ Likelihood.py
@@ -12,7 +12,7 @@ def multiplier(num1, num2=1):
 		start -= 1 #Decrease larger number by 1
 	return product #Return product
 		
-def bincoeffb(n, k):
+def bincoeffb(k, n):
 	""" Calculate binomial coefficient (factoring first) from n sets choosing k subsets"""
 	try:
 		coeff = multiplier(n, n-k+1)/multiplier(k)
@@ -22,34 +22,39 @@ def bincoeffb(n, k):
 	
 def bernoulli(k, n, p):
 	"""Calculate a Probability Mass Function (PMF) of a binomial distribution with k successes in n Bernoulli trials with probability p"""
-	PMF = bincoeffb(n, k)*pow(p,k)*pow((1-p),(n-k))
+	PMF = bincoeffb(k, n)*pow(p,k)*pow((1-p),(n-k))
 	return PMF
-	
+
+def likeli (k, n, p):
+	"""Calculate likelihood"""
+	likelival = bernoulli(k, n, p)/bincoeffb(k, n)
+	return likelival
 
 """Enter data below as pass and fail separated by commas"""
-data = ["pass","pass","pass","pass","pass"]
+data = ["pass","pass","pass","pass","fail"]
 data = [y.lower() for y in data]
-dataval = data.count("pass")
-numTrials = len(data)
+#dataval = data.count("pass")
+#numTrials = len(data)
+dataval = 12
+numTrials = 20
 
 list = [x/100 for x in range(0,101,5)] #List with probabilities in range 0 to 1 spaced by 0.05
 
-likeli = []
+likelist = []
 
 for val in list: #Calculate likelihoods
-	num = bernoulli(dataval, numTrials, val)
-	likeli.append(num)
+	num = likeli(dataval,numTrials,val)
+	likelist.append(num)
 
-maxval = max(likeli)
-print "\nThe maximum likelihood is {0} for data with probability {1}\n".format(round(maxval,4), list[likeli.index(maxval)])
+maxval = max(likelist)
+print "\nThe maximum likelihood is {0:.4e} for data with probability {1}\n".format(maxval, list[likelist.index(maxval)])
 
 for numbers in range(0,len(list)): #Print data
-	print "{0:0=3d}% balls black \t likelihood = {1:.4f} \t likelihood ratio = {2:.4f}".format(int(list[numbers]*100), round(likeli[numbers],4), round(likeli[numbers]/maxval, 4))
-
+	print "{0:0=3d}% balls black \t likelihood = {1:.4e} \t ratio = {2:.4f}".format(int(list[numbers]*100), likelist[numbers], round(likelist[numbers]/maxval, 4))
 
 input = raw_input("Graph data (Y/N)") #Graph data Y/N
 if input.upper() == "Y": 
-	plt.plot(list, likeli)
+	plt.plot(list, likelist)
 	plt.xlabel("Probability")
 	plt.ylabel("Likelihood")
 	plt.show()

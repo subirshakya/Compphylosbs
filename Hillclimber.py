0850256afa924ef2565d2dc2e7bcaa47a97e6de3
Subir <subirshakya@users.noreply.github.com>
2015-02-02 14:49:20 -0600

    More small changes to counter errors
    Still not perfect

--- Hillclimber.py
+++ Hillclimber.py
@@ -24,7 +24,7 @@ def bernoulli(k, n, p):
 	PMF = bincoeffb(n, k)*pow(p,k)*pow((1-p),(n-k))
 	return PMF
 
-def hillclimb(data, prob, likeli=0, diff = 0.1, oldvalue=1):
+def hillclimb(data, prob, likeli=0, diff = 0.1, oldvalue=1, direction="RIGHT"):
 	"""Calculate maximum likelihood for a dataset"""
 	while diff >= 0.001: #Terminator loop
 		if likeli == oldvalue: #Terminate recursion if values equal and change difference parameter to start new recursive list
@@ -32,16 +32,19 @@ def hillclimb(data, prob, likeli=0, diff = 0.1, oldvalue=1):
 			prob = prob
 			diff = diff/5
 			oldvalue = 1
-			hillclimb(data, prob=prob, likeli=likeli, diff=diff, oldvalue=oldvalue)
+			hillclimb(data, prob=prob, likeli=likeli, diff=diff, oldvalue=oldvalue, direction="RIGHT")
 		else: #Recurvise function to look for maximum point
 			oldvalue = likeli
 			problist = [prob-diff, prob, prob+diff]
 			valuelist = [bernoulli(data[0], data[1], prob-diff), bernoulli(data[0], data[1], prob), bernoulli(data[0], data[1], prob+diff)]
+			if direction == "LEFT":
+				problist = problist[::-1]
+				valuelist = valuelist[::-1]
 			likeli = max(valuelist) #Choose maximum value of likelihood
-			if valuelist[0] == 0 and valuelist[1] == 0: #The program always goes left if two values are equal due to the max function. THis shifts it right.
-				likeli = valuelist[2]
 			prob = problist[valuelist.index(likeli)]
-			hillclimb(data, prob=prob, likeli=likeli, diff=diff, oldvalue=oldvalue)
+			if prob<=0: #The program always goes left if two values are equal due to the max function. THis shifts it right.
+				direction = "LEFT"
+			hillclimb(data, prob=prob, likeli=likeli, diff=diff, oldvalue=oldvalue, direction=direction)
 	return likeli, prob
 	
 """Enter data below as pass and fail separated by commas"""

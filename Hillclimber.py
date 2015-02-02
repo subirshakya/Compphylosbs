328a38bcbc2c7e45a66187fd7fb1b0caecdf6553
Subir <subirshakya@users.noreply.github.com>
2015-02-02 14:30:04 -0600

    Optimized for zero values

--- Hillclimber.py
+++ Hillclimber.py
@@ -26,18 +26,20 @@ def bernoulli(k, n, p):
 
 def hillclimb(data, prob, likeli=0, diff = 0.1, oldvalue=1):
 	"""Calculate maximum likelihood for a dataset"""
-	while diff >= 0.001:
-		if likeli == oldvalue:
+	while diff >= 0.001: #Terminator loop
+		if likeli == oldvalue: #Terminate recursion if values equal and change difference parameter to start new recursive list
 			likeli = likeli
 			prob = prob
 			diff = diff/5
 			oldvalue = 1
 			hillclimb(data, prob=prob, likeli=likeli, diff=diff, oldvalue=oldvalue)
-		else:
+		else: #Recurvise function to look for maximum point
 			oldvalue = likeli
 			problist = [prob-diff, prob, prob+diff]
 			valuelist = [bernoulli(data[0], data[1], prob-diff), bernoulli(data[0], data[1], prob), bernoulli(data[0], data[1], prob+diff)]
-			likeli = max(valuelist)
+			likeli = max(valuelist) #Choose maximum value of likelihood
+			if valuelist[0] == 0 and valuelist[1] == 0: #The program always goes left if two values are equal due to the max function. THis shifts it right.
+				likeli = valuelist[2]
 			prob = problist[valuelist.index(likeli)]
 			hillclimb(data, prob=prob, likeli=likeli, diff=diff, oldvalue=oldvalue)
 	return likeli, prob
@@ -49,13 +51,13 @@ dataval = data.count("pass")
 numTrials = len(data)
 data = (dataval, numTrials)
 
-if data[0] == data[1]:
+if data[0] == data[1]: #Some special cases
 	maxlikeli,  probability = 1.0, 1.0
-elif data[0] == 0:
+elif data[0] == 0: # Special case 2
 	maxlikeli, probability = 1.0, 0.0
 else:
 	startlist = [y/10 for y in range(0,10,1)]
-	berns = [bernoulli(data[0],data[1], y) for y in startlist]
-	prob = startlist[berns.index(random.choice(berns))]
+	berns = [bernoulli(data[0],data[1], y) for y in startlist] 
+	prob = startlist[berns.index(random.choice(berns))] #To randomly choose starting probability
 	maxlikeli, probability = hillclimb(data, prob)
 print "The maximum likelihood for given data is {0} for probability of {1}".format(round(maxlikeli, 5), round(probability, 5))

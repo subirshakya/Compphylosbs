cf5e690c076703dc31b431893c2e16e95650b8c2
Subir <subirshakya@users.noreply.github.com>
2015-02-09 16:22:57 -0600

    Markov chains

---
+++ Markov.py
@@ -0,0 +1,45 @@
+from __future__ import division
+import scipy
+import time
+
+#Discrete sampling
+def sample(list, prob, outputsize = 1):
+	"""Resamples with replacement from original discrete distribution to give newlist"""
+	output = []
+	for val in range(outputsize):
+		cumfreq = []
+		y = 0
+		for i in range(len(prob)):
+			cumfreq.append(y + prob[i])
+			y = cumfreq[i]
+		rand = scipy.random.random()
+		for i in cumfreq:
+			if rand <= i:
+				output.append(list[cumfreq.index(i)])
+				break
+	return output
+
+def Markov(list, condMat, steps, start, timed = "yes"):
+	"""Processes a Markov chain from a list to a defined number of steps"""
+	output = []
+	while steps!=0:
+		problist = condMat[list.index(start)]
+		next = sample(list, problist)
+		start = next[0]
+		steps-=1
+		if timed == "yes":
+			print next[0],
+			time.sleep(1)
+		else:
+			output.append(next[0])
+	return output
+		
+list = ("A", "B") #Provide list of names
+InitProb = (0, 1) # Provide probabilities of items
+
+#Define conditional matrix:
+CondMat = [[0 for y in range(len(InitProb))] for y in range(len(InitProb))]
+CondMat[0][0], CondMat[0][1], CondMat[1][0], CondMat[1][1] = 0.50, 0.50, 0.50, 0.90 #pA2A, pA2B, pB2A, pB2B
+start = scipy.random.choice(list)
+Markov(list, CondMat, 25, start)
+#print Markov(list, CondMat, 25, start, timed="no")

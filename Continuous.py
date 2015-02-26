0fcfbd07df395e554901c637531bc532b4b466e4
Subir <subirshakya@users.noreply.github.com>
2015-02-26 10:56:07 -0600

    Update Continuous.py

--- Continuous.py
+++ Continuous.py
@@ -1,6 +1,8 @@
 from __future__ import division
 from __future__ import print_function
 import random
+from scipy.linalg import expm
+import numpy
 import math
 
 class SequenceEvo(object):
@@ -31,11 +33,11 @@ class Markov(SequenceEvo):
 		for val in range(outputsize):
 			cumfreq = []
 			y = 0
-			for i in range(len(prob)):
+			for i in range(len(prob)): #Calculate cumulative frequency list
 				cumfreq.append(y + prob[i])
 				y = cumfreq[i]
 			rand = random.random()
-			for i in cumfreq:
+			for i in cumfreq: #Discrete sampling
 				if rand <= i:
 					output.append(list[cumfreq.index(i)])
 					break
@@ -70,9 +72,9 @@ class ContMarkov(SequenceEvo):
 			states.append(start)
 			timevals.append(time_elapsed)			
 			time_elapsed = (-1/self.diag[start])*math.log(random.random()) #Calculate length of time after next instance
-			list = filter(lambda x:x != start, self.sequence) #Get list of variables it can change to
-			prob = filter(lambda x:x != -1, self.values[start]) #Get list of probabilities for these variables
-			nextval = Markov().sample(list, prob) #Samples new distribution
+			listvals = list(filter(lambda x:x != start, self.sequence)) #Get list of variables it can change to
+			prob = list(filter(lambda x:x != -1, self.values[start])) #Get list of probabilities for these variables
+			nextval = Markov().sample(listvals, prob) #Samples new distribution
 			time_passed += time_elapsed #Calculate time elapsed
 			start = nextval[0] #Reset the starting variable
 		return states, timevals
@@ -86,12 +88,13 @@ class ContMarkov(SequenceEvo):
 		print ("-"*int((self.time-sum(timevals))*multiplier/2))
 		
 	def endFreqs(self, start, trials):
+		#Calculate end frequencies after n trials
 		endvals = []
 		for x in range(trials):
 			output1, output2 = model.simulation(start=start)
-			endvals.append(output1[len(output1)-1])
+			endvals.append(output1[len(output1)-1]) #Get last value after simulation
 		freqlist = {}
-		for val in endvals:
+		for val in endvals: #Calculate frequencies of each value
 			if val in freqlist:
 				freqlist[val]+=1
 			else:
@@ -99,14 +102,23 @@ class ContMarkov(SequenceEvo):
 		frequencies = []
 		for val in self.sequence:
 			frequencies.append(freqlist[val]/1000)
-		print (frequencies)
+		return frequencies
 		
+	def calcHistProb(self, states, timevals):
+		#Calculate historical probabilities
+		pass
+	
+	def calcMargProb(self):
+		#Calculate Marginal Probabilities of each outcome
+		return (expm((numpy.array(self.matrix) * self.time)))
+			
 #statetime, endFreqs, calcHistProb, calcMargProb functions
 #Use function scipy.linalg.expm(Qv)
 		
-model = ContMarkov(["A", "C", "G", "T"], [[-1.916, 0.541, 0.787, 0.588], [0.148, -1.069, 0.415, 0.506], [0.286, 0.170, -0.591, 0.135], [0.525, 0.236,0.594, -1.355]], time = 10)
+model = ContMarkov(sequence = ["A", "C", "G", "T"], matrix = [[-1.916, 0.541, 0.787, 0.588], [0.148, -1.069, 0.415, 0.506], [0.286, 0.170, -0.591, 0.135], [0.525, 0.236, 0.594, -1.355]], time = 10)
 #states, times = model.simulation() 
 #print (states) # Use this to print the states through time
 #print (states, times) #Use this to print states and times
 #model.drawsim(states, times) #Use to visualize states along timeline
-#model.endFreqs(start = "A", trials=1000) #Use this to calculate frequency of an allele after certain generations
+#print (model.endFreqs(start = "A", trials=1000)) #Use this to calculate frequency of an allele after certain generations
+#print (model.calcMargProb()) #Use this to print the final stationary probabilities after given time 

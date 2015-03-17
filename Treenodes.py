d7075409da09f976c0aff97480ebef5cde02c719
Subir <subirshakya@users.noreply.github.com>
2015-03-17 14:40:01 -0500

    Everything including parsing parenthetical trees works

--- Treenodes.py
+++ Treenodes.py
@@ -1,10 +1,10 @@
 from __future__ import (division, print_function)
-import ctmc
+import ctmc #Need to be in python folder Lib
 
 # ---> Defining Node and Tree classes <---
 class Node:
 
-	def __init__(self,name="",parent=None,children=None, branchlength = 0):
+	def __init__(self,name="",parent=None,children=None, branchlength = 0, sequence = None):
 		self.name = name
 		self.parent = None
 		if children is None:
@@ -12,13 +12,17 @@ class Node:
 		else:
 			self.children = children
 		self.brl = branchlength
+		if sequence is None:
+			self.sequence = []
+		else:
+			self.sequence = sequence
 
 class Tree:
 	"""
 	Defines a class of phylogenetic tree, consisting of linked Node objects.
 	"""
 	
-	def __init__(self):
+	def __init__(self, data):
 		"""
 		The constructor really needs to be more flexible, but for now we're 
 		going to define the whole tree structure by hand. This just uses
@@ -26,32 +30,40 @@ class Tree:
 		write a constructor that takes a parenthetical tree as its argument and 
 		builds the corresponding tree in memory. 
 		"""
+
+		self.root = Node("root") #Define root
+		self.newicksplicer(data, self.root) #Splice newick data
+		self.setModels(self.root) #Default seqlength is 50, you can add any seqlength, default Model is JK, you can add model matrix
 		
-		self.root = Node("root") 
-		self.spC = Node("SpeciesC",parent=self.root)
-		self.root.children.append(self.spC)
-		self.ancAB = Node("ancAB",parent=self.root)
-		self.root.children.append(self.ancAB)
-		self.spA = Node("SpeciesA",parent=self.ancAB)
-		self.spB = Node("SpeciesB",parent=self.ancAB)
-		self.ancAB.children.append(self.spA)
-		self.ancAB.children.append(self.spB)
-		# Now, let's add branch lengths to our Node objects (remember, these fields
-		# can be added arbitrarily in Python). In the future, we should probably include
-		# branch lengths in the Node constructor.
-		self.spA.brl = 0.1
-		self.spB.brl = 0.1
-		self.spC.brl = 0.2
-		self.ancAB.brl = 0.1
-		self.root.brl = 0
-		# We're also going to add lists to each node that will hold simulated sequences.
-		self.spA.seq = []
-		self.spB.seq = []
-		self.spC.seq = []
-		self.ancAB.seq = []
-		self.root.seq = []
-		self.setModels(self.root)
+	def newicksplicer(self, data, base): 
+		""" 
+		Splices newick data to create a node based tree. Takes a base argument which is the root node
+		"""
 		
+		data = data.replace(" ", "")[1: len(data)] 	 #Get rid of all spaces and removes first and last parenthesis
+		n = 0
+		if data.count(",") != 0: #While there is no more comma separated taxa
+			for key in range(len(data)): #Find the corresponding comma for a given parenthesis (n will be 0 for the correct comma)
+				if data[key] == "(":
+					n += 1 #Increase index of n by 1 for 1 step into new node
+				elif data[key] == ")":
+					n -= 1 #Decrease index of n by 1 for 1 step outout node
+				elif data[key] == ",": 
+					if n == 0: #To check for correct comma
+						vals = (data[0:key], data[key+1:len(data)-1]) #Break newick into left and right datasets
+						for unit in vals: #For each entry of dataset
+							if unit[-1] != ")": #For cases with branch lengths
+								data = unit[0:unit.rfind(":")] #get rid of trailing branchlength if provided
+								node_creater = Node(data, parent = base) #Create node entry
+								node_creater.brl = float(unit[unit.rfind(":")+1:]) #Append branch length of that branch
+								base.children.append(node_creater) #Create children
+								self.newicksplicer(data, node_creater) #Recursive function
+							else: #For case with no branch lengths
+								node_creater = Node(data, parent = base)
+								base.children.append(node_creater)
+								self.newicksplicer(data, node_creater)
+						break #Terminate loop, we don't need to look any further
+	
 	def printNames(self,node):
 		"""
 		A method of a Tree object that will print out the names of its
@@ -68,6 +80,7 @@ class Tree:
 		"""
 		A method to calculate and return total tree length.
 		"""
+		
 		tot_len = 0 
 		if node.children == []: #Terminal branch returns branch length
 			return node.brl
@@ -82,6 +95,7 @@ class Tree:
 		A method of a Tree object that will print out the Tree as a 
 		parenthetical string (Newick format).
 		"""
+		
 		newick = "(" #Opening bracket
 		if node.children == []: #Terminal branch returns name
 			return node.name + ":" + str(node.brl)
@@ -95,55 +109,52 @@ class Tree:
 				newick += "):" + str(node.brl) #Adds closing bracket
 			else:
 				newick += ")"
-			return newick
-
-
-	# Now, let's write a recursive function to simulate sequence evolution along a
-	# tree. This amounts to simply simulating evolution along each branch 
-	# from the root towards the tips. We'll need to use our ctmc class for setting the 
-	# conditions of our simulation, which is why we imported it above our tree 
-	# class definition. In this case, we've stored the definition of our ctmc 
-	# class in a separate file (ctmc.py) to keep our tree code compact.
-	# Now, let's add a ctmc object to each internal node in our tree (except the
-	# root). Again, it would be best to add the ctmcs as part of the Node
-	# constructor, if we know that we'll be simulating data.
+			return newick    
 	
-	# Try to get this simulator and associated functions working by next Thurs. (3/19)    
-	
-	def setModels(self,node):
+	def setModels(self,node, Model = None, seqlength = None):
 		"""
 		This method of a Tree object defines a ctmc object associated with all
 		nodes that have a branch length (i.e., all but the root).
 		"""
-		self.root.seq = [ctmc.ContMarkov().simulation()]
-		self.spC.seq = [ctmc.ContMarkov(time = self.spC.brl).simulation(seq=self.root.seq)]
-		self.ancAB.seq = [ctmc.ContMarkov(time = self.ancAB.brl).simulation(seq=self.root.seq)]
-		self.spA.seq = [ctmc.ContMarkov(time = self.spA.brl).simulation(seq=self.ancAB.seq)]
-		self.spB.seq = [ctmc.ContMarkov(time = self.spB.brl).simulation(seq=self.ancAB.seq)]
+		
+		if Model is None and seqlength is None: #Set starting sequence for root and change model or sequence length if required
+			node.sequence = [ctmc.ContMarkov().simulation()]
+		elif Model is not None and seqlength is None:
+			node.sequence = [ctmc.ContMarkov(model = Model).simulation()]
+		elif Model is None and seqlength is not None:
+			node.sequence = [ctmc.ContMarkov().simulation(seql=seqlength)]
+		else:
+			node.sequence = [ctmc.ContMarkov(model = Model).simulation(seql=seqlength)]
+		self.simulate(node)
 
 	def simulate(self,node):
 		"""
 		This method simulates evolution along the branches of a tree, taking
 		the root node as its initial argument.
 		"""
-		pass
 		
-	def printSeqs(self):
+		if node.children == []:
+			pass
+		else:
+			for child in node.children:
+				child.sequence = [ctmc.ContMarkov(time = child.brl).simulation(seq=node.sequence)]
+				self.simulate(child)
+		
+	def printSeqs(self, node):
 		"""
 		This method prints out the names of the tips and their associated
 		sequences as an alignment (matrix).
 		"""
-		print (self.root.seq)
-		print (self.spC.seq)
-		print (self.ancAB.seq)
-		print (self.spA.seq)
-		print (self.spB.seq)
+		
+		if node.children == []:
+			print (node.name, "\t", node.sequence)
+		for child in node.children:
+			self.printSeqs(child)
+		
+data = "((Species A:1, Species B:1):2 , (Species C: 3, (Species D:4, Species E:4):5):6)"
 
-#data = "(Species C:0.2, (Species A:0.1, Species B: 0.1):0.1)"
-#print (newicksplicer(data))
-
-Sim = Tree()
-Sim.printSeqs()
+Sim = Tree(data)
+#Sim.printSeqs(Sim.root)
 #Sim.printNames(Sim.root)
 #print (Sim.treeLength(Sim.root))
 #print (Sim.newick(Sim.root))
